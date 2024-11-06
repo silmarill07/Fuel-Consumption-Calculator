f8ac8015b06b50fe16ca2dc7ef2d01c7c5795a6b
silmarill07 <56761613+silmarill07@users.noreply.github.com>
2024-11-06 18:12:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ function calculateTripCost() {
     const fuelPrice = parseFloat(document.getElementById("fuelPrice").value);
     if (distance > 0 && fuelPer100km > 0 && fuelPrice > 0) {
         const tripCost = (distance / 100) * fuelPer100km * fuelPrice;
-        document.getElementById("tripCostResult").innerText = `Стоимость поездки: ${tripCost.toFixed(2)} грн`;
+        document.getElementById("tripCostResult").innerText = `Вартість поїздки: ${tripCost.toFixed(2)} грн`;
     } else {
-        document.getElementById("tripCostResult").innerText = "Введите корректные данные";
+        document.getElementById("tripCostResult").innerText = "Введіть коректні дані";
     }
 }
 
@@ -40,9 +40,9 @@ function calculateFuelConsumption() {
     const fuelUsed = parseFloat(document.getElementById("fuelUsed1").value);
     if (distance > 0 && fuelUsed > 0) {
         const fuelConsumption = (fuelUsed / distance) * 100;
-        document.getElementById("fuelConsumptionResult").innerText = `Расход топлива: ${fuelConsumption.toFixed(2)} л/100 км`;
+        document.getElementById("fuelConsumptionResult").innerText = `Витрата палива: ${fuelConsumption.toFixed(2)} л/100 км`;
     } else {
-        document.getElementById("fuelConsumptionResult").innerText = "Введите корректные данные";
+        document.getElementById("fuelConsumptionResult").innerText = "Введіть коректні дані";
     }
 }
 
@@ -52,8 +52,8 @@ function calculateDistance() {
     const fuelPer100km = parseFloat(document.getElementById("fuelPer100km2").value);
     if (availableFuel > 0 && fuelPer100km > 0) {
         const distance = (availableFuel / fuelPer100km) * 100;
-        document.getElementById("distanceResult").innerText = `Возможное расстояние: ${distance.toFixed(2)} км`;
+        document.getElementById("distanceResult").innerText = `Можлива відстань: ${distance.toFixed(2)} км`;
     } else {
-        document.getElementById("distanceResult").innerText = "Введите корректные данные";
+        document.getElementById("distanceResult").innerText = "Введіть коректні дані";
     }
 }
